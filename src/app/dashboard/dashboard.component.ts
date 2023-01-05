825c94d08dc952c083655c58d2e36907058d3ab3
unknown <manvendrasrmcem.1996@gmail.com>
2023-01-05 12:11:14 +0530

    addding the 2 charts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,8 +3,7 @@ import { Component,AfterViewInit, ViewChild, ElementRef,OnInit } from '@angular/
 import * as echarts from 'echarts';
 
 
-const chartElement = document.getElementById('chart-container');
-console.log( "chartElement :",chartElement);
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -143,6 +142,183 @@ option3 :echarts.EChartsOption = {
         { value: 1048, name: 'Search Engine' },
         { value: 735, name: 'Direct' },
       ]
+    }
+  ]
+};
+
+// !....................Bar Charts for Overall Inspection...................
+
+option4 :echarts.EChartsOption = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: [
+    {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      axisTick: {
+        alignWithLabel: true
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value'
+    }
+  ],
+  series: [
+    {
+      name: 'Direct',
+      type: 'bar',
+      barWidth: '60%',
+      data: [10, 52, 200, 334, 390, 330, 220]
+    }
+  ]
+};
+
+// ! ................. Normal Bar chart......................
+
+// option4 :echarts.EChartsOption = {
+//   tooltip: {
+//     trigger: 'axis',
+//     axisPointer: {
+//       type: 'shadow'
+//     }
+//   },
+//   grid: {
+//     left: '3%',
+//     right: '4%',
+//     bottom: '3%',
+//     containLabel: true
+//   },
+//   xAxis: [
+//     {
+//       type: 'category',
+//       data: ['Combi', 'Rear Lamp', 'Front Lamp', 'Rear Spec', 'Front Spec'],
+//       axisTick: {
+//         alignWithLabel: true
+//       }
+//     }
+//   ],
+//   yAxis: [
+//     {
+//       type: 'value'
+//     }
+//   ],
+//   series: [
+//     {
+//       name: 'Direct',
+//       type: 'bar',
+//       barWidth: '60%',
+//       data: [10, 52, 200, 334, 390, 330, 220]
+//     }
+//   ]
+// };
+
+// !................Line Graph which shows History Logs...........
+
+option5 :echarts.EChartsOption = {
+  title: {
+    text: 'Stacked Area Chart'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      label: {
+        backgroundColor: '#6a7985'
+      }
+    }
+  },
+  legend: {
+    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
+    }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: [
+    {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Combi', 'Rear Lamp', 'Front Lamp', 'Rear Spec', 'Front Spec']
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value'
+    }
+  ],
+  series: [
+    {
+      name: 'Email',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series'
+      },
+      data: [120, 132, 101, 134, 90, 230, 210]
+    },
+    {
+      name: 'Union Ads',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series'
+      },
+      data: [220, 182, 191, 234, 290, 330, 310]
+    },
+    {
+      name: 'Video Ads',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series'
+      },
+      data: [150, 232, 201, 154, 190, 330, 410]
+    },
+    {
+      name: 'Direct',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series'
+      },
+      data: [320, 332, 301, 334, 390, 330, 320]
+    },
+    {
+      name: 'Search Engine',
+      type: 'line',
+      stack: 'Total',
+      label: {
+        show: true,
+        position: 'top'
+      },
+      areaStyle: {},
+      emphasis: {
+        focus: 'series'
+      },
+      data: [820, 932, 901, 934, 1290, 1330, 1320]
     }
   ]
 };
